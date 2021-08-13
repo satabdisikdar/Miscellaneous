8ac0efc8d98d264900864b1c8d14d2de4f2c9dd0
satabdisikdar <satabdi.sikdar@velotio.com>
2021-08-13 21:08:18 +0530

    template

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,7 @@
 terraform {
-  backend "remote" {
-    organization = "QA_FY"
-    workspaces {
-      name = "Miscellaneous"
-    }
-  }
+  required_version = ">= 0.12.26"
+}
+# website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
+output "hello_world" {
+  value = "Hello, World!"
 }
