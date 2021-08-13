e15ae8fd50ce730554a3d85023b2c7bcb959029a
satabdisikdar <satabdi.sikdar@velotio.com>
2021-08-13 12:21:26 +0530

    Python sample

--- pythonThings/testPython.py
+++ pythonThings/testPython.py
@@ -8,10 +8,10 @@ class Employee:
       Employee.empCount += 1
    
    def displayCount(self):
-     print "Total Employee %d" % Employee.empCount
+     print("Total Employee %d" % Employee.empCount)
 
    def displayEmployee(self):
-      print "Name : ", self.name,  ", Salary: ", self.salary
+      print("Name : ", self.name,  ", Salary: ", self.salary)
 
 "This would create first object of Employee class"
 emp1 = Employee("Zara", 2000)
@@ -19,4 +19,4 @@ emp1 = Employee("Zara", 2000)
 emp2 = Employee("Manni", 5000)
 emp1.displayEmployee()
 emp2.displayEmployee()
-print "Total Employee %d" % Employee.empCount
+print("Total Employee %d" % Employee.empCount)
