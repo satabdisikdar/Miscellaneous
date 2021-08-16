4608b82ac44c5a9a990b750e6eb9fefad7f671e0
satabdisikdar <satabdi.sikdar@velotio.com>
2021-08-16 15:29:55 +0530

    file updated

--- terraform.tf
+++ terraform.tf
@@ -2,6 +2,6 @@ terraform {
   required_version = ">= 0.12.26"
 }
 # website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
-output "hello_world" {
-  value = "Hello, World!"
+output "hello_everyone" {
+  value = "Hello, Everyone! How are you doing?"
 }
